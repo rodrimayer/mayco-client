8290e477f775075758b70f42095bb27d7d8ec76d
Nicolas Bloj <nicolasbloj@gmail.com>
2017-09-20 00:50:57 -0300

    company and home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { CoreModule } from './core/core.module';
 import { ShellComponent } from './core/shell/shell.component';
+import { CompanyComponent } from './company/company.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [CompanyComponent, HomeComponent],
   imports: [
     BrowserModule,
     CoreModule
---
+++ src/app/company/company.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nr-company',
+  templateUrl: './company.component.html',
+  styles: []
+})
+export class CompanyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
