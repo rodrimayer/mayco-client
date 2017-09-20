4d633e34b2020022495b8030ccaf822e757b9f5f
Nicolas Bloj <nicolasbloj@gmail.com>
2017-09-20 01:18:28 -0300

    routing company and home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { CoreModule } from './core/core.module';
 import { ShellComponent } from './core/shell/shell.component';
-import { CompanyComponent } from './company/company.component';
-import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
-  declarations: [CompanyComponent, HomeComponent],
+  declarations: [],
   imports: [
     BrowserModule,
     CoreModule
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { ShellComponent } from './shell/shell.component';
 import { MainContentComponent } from './shell/main-content/main-content.component';
 import { HeaderComponent } from './shell/header/header.component';
 import { FooterComponent } from './shell/footer/footer.component';
+import { CoreRoutingModule } from './core-routing.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    CoreRoutingModule
   ],
   declarations: [ShellComponent, MainContentComponent, HeaderComponent, FooterComponent],
   exports: [ShellComponent]
