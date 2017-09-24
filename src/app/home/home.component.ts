969215962cb435eedf03c28fc10e0df681f041bf
rodrimayer <rodrimmayer@gmail.com>
2017-09-23 22:06:25 -0300

    prueba ruta imagen3

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   //PATH_LOGO = '../../assets/logo.png';
-  PATH_LOGO = '../../../dist/assets/logo.png';
+  PATH_LOGO  = 'src/assets/logo.png';
   constructor() { }
 
   ngOnInit() {
